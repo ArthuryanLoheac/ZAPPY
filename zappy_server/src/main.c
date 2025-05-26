3ac3c5673c789a4e7e0f939c2ce52ee2a332aa7b
Leopold-PINTARD <leopold.pintard@epitech.eu>
2025-05-26 12:04:54 +0200

    🧱 Add folder for each part of the project

---
+++ zappy_server/src/main.c
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2025
+** ZAPPY
+** File description:
+** main
+*/
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+    printf("Hello world");
+    return 0;
+}

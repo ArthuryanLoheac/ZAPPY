c036b7c9884e80883cd1b6c9c4cfdc8af0601d12
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-05-28 12:11:06 +0200

    ✨ Enhance ServerGuiConnection with command parsing and handling

--- zappy_gui_src/Connection/ServerGuiConnection.cpp
+++ zappy_gui_src/Connection/ServerGuiConnection.cpp
@@ -3,8 +3,48 @@
 
 namespace GUI
 {
+ServerGuiConnection::ServerGuiConnection()
+{
+}
+
 void GUI::ServerGuiConnection::handleCommand(std::string &command)
 {
-    std::cout << "Command received and handled : " << command << std::endl;
+    try {
+        std::vector<std::string> args = parseCommands(command);
+        if (args.empty())
+            throw std::runtime_error("No command provided");
+        std::string commandName = args[0];
+        auto it = commands.find(commandName);
+        if (it != commands.end()) {
+            (this->*(it->second))(args);
+        } else {
+            std::cerr << "Unknown command: " << commandName << std::endl;
+        }
+    } catch (const std::exception &e) {
+        std::cerr << "Error handling command: " << e.what() << std::endl;
+    }
+}
+
+std::vector<std::string> ServerGuiConnection::parseCommands(std::string &command)
+{
+    std::vector<std::string> args;
+    size_t pos = 0;
+    while ((pos = command.find(' ')) != std::string::npos) {
+        args.push_back(command.substr(0, pos));
+        command.erase(0, pos + 1);
+    }
+    args.push_back(command); // Add the last part of the command
+    if (args.empty()) {
+        std::cerr << "Empty command received." << std::endl;
+        throw std::runtime_error("Empty command received");
+    }
+    return args;
+}
+
+void GUI::ServerGuiConnection::welcomeCommand(std::vector<std::string> &args)
+{
+    for (const auto &arg : args) {
+        std::cout << "WELCOME argument: " << arg << std::endl;
+    }
 }
 } // namespace GUI
--- zappy_gui_src/Connection/ServerGuiConnection.hpp
+++ zappy_gui_src/Connection/ServerGuiConnection.hpp
@@ -1,15 +1,26 @@
 #pragma once
 #include <string>
+#include <map>
+#include <vector>
 
 namespace GUI
 {
 class ServerGuiConnection {
  public:
+    ServerGuiConnection();
     static ServerGuiConnection &i() {
         static ServerGuiConnection i;
         return i;
     }
     void handleCommand(std::string &command);
+ private:
+    std::vector<std::string> parseCommands(std::string &command);
+
+    std::map<std::string, void (ServerGuiConnection::*)(std::vector<std::string> &)> commands = {
+        {"WELCOME", &ServerGuiConnection::welcomeCommand},
+    };
+
+    void welcomeCommand(std::vector<std::string> &args);
 };
 
 } // namespace GUI

28377f0023bd2dda54bd751e6cf0fafa057932f0
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-05-29 00:55:38 +0200

    ✨ Implement camera rotation and movement adjustments

--- zappy_gui_src/Graphic/Window/WindowEvent.cpp
+++ zappy_gui_src/Graphic/Window/WindowEvent.cpp
@@ -25,25 +25,31 @@ void Window::updateDeltaTime() {
 }
 
 void Window::moveCamera(float x, float zoom, float xMove, float yMove) {
+    // rotate around
+    angleXCamera += x * frameDeltaTime * rotationSpeedCamera;
+    if (angleXCamera > 360.f) angleXCamera -= 360.f;
+    if (angleXCamera < 0.f) angleXCamera += 360.f;
+    float radAngleX = std::cos(angleXCamera * (M_PI / 180.0f));
+    float radAngleZ = std::sin(angleXCamera * (M_PI / 180.0f));
+    irr::core::vector3df pos(
+        distanceFromCenter * radAngleX, 1,
+        distanceFromCenter * radAngleZ);
+
     // move Origin
     int width = GUI::GameDataManager::i().getWidth();
     int height = GUI::GameDataManager::i().getHeight();
 
-
     irr::core::vector3df posTarget = cam->getTarget();
+    float rotatedXMove = xMove * radAngleX - yMove * radAngleZ;
+    float rotatedYMove = xMove * radAngleZ + yMove * radAngleX;
+    xMove = rotatedXMove;
+    yMove = rotatedYMove;
     posTarget.X += xMove * frameDeltaTime * moveSpeedCamera;
     posTarget.Z += yMove * frameDeltaTime * moveSpeedCamera;
     posTarget.X = std::clamp(posTarget.X, -(width / 2.f), (width / 2.f));
     posTarget.Z = std::clamp(posTarget.Z, -(height / 2.f), (height / 2.f));
     cam->setTarget(posTarget);
-    // rotate around
-    angleXCamera += x * frameDeltaTime * rotationSpeedCamera;
-    if (angleXCamera > 360.f) angleXCamera -= 360.f;
-    if (angleXCamera < 0.f) angleXCamera += 360.f;
-    irr::core::vector3df pos(
-        distanceFromCenter * std::cos(angleXCamera * (M_PI / 180.0f)),
-        1,
-        distanceFromCenter * std::sin(angleXCamera * (M_PI / 180.0f)));
+
     // Zoom
     distanceFromCenter += zoom * frameDeltaTime * zoomSpeedCamera;
     distanceFromCenter = std::clamp(distanceFromCenter, 1.f, 50.f);

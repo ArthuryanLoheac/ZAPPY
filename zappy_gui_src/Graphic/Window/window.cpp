56a779330307378d3a53b325bb2da17d2dc55fb4
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-05-28 18:53:46 +0200

    :sparkles: Add camera setup and animated mesh to the window initialization

--- zappy_gui_src/Graphic/Window/window.cpp
+++ zappy_gui_src/Graphic/Window/window.cpp
@@ -10,10 +10,26 @@ Window::Window() {
     driver = device->getVideoDriver();
     smgr = device->getSceneManager();
     guienv = device->getGUIEnvironment();
+    // Text
     guienv->addStaticText(L"Hello World! This is the ZAPPY!",
         irr::core::rect<irr::s32>(10,10,260,22), true);
-    smgr->addCameraSceneNode(0, irr::core::vector3df(0,30,-40),
+    // Camera
+    smgr->addCameraSceneNode(0, irr::core::vector3df(0,0,-10),
         irr::core::vector3df(0,5,0));
+
+    // Test object
+    irr::scene::IAnimatedMesh* mesh = smgr->getMesh("assets/Drone.obj");
+    if (!mesh) {
+        device->drop();
+        return;
+    }
+    irr::scene::IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(mesh);
+    if (node)
+    {
+        node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+        node->setMD2Animation(irr::scene::EMAT_STAND);
+        node->setMaterialTexture(0, driver->getTexture("assets/Drone.bmp"));
+    }
 }
 
 void Window::update() {

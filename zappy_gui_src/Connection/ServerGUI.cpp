b26886ba7812c43533e87a8b1cbf867735231eed
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-06-12 18:39:36 +0200

    ✨ Enhance command logging and debug output; improve color definitions for team meshes

--- zappy_gui_src/Connection/ServerGUI.cpp
+++ zappy_gui_src/Connection/ServerGUI.cpp
@@ -44,8 +44,10 @@ void ServerGUI::execCommand(std::map<std::string, void(GUI::ServerGUI::*)
     if (it != commands.end()) {
         try {
             (GUI::ServerGUI::i().*(it->second))(args);
-            LOG_DEBUG(("\033[1;32m[OK]\033[0m Received Command: "
-                + args[0]).c_str());
+            std::string logMessage = "\033[1;32m[OK]\033[0m Received Command: " + args[0];
+            for (size_t i = 1; i < args.size(); i++)
+                logMessage += " " + args[i];
+            LOG_DEBUG(logMessage.c_str());
         } catch (const std::exception &e) {
             errStr += std::string(e.what()) + " : ";
             for (size_t i = 0; i < args.size(); i++)
@@ -79,7 +81,7 @@ std::chrono::_V2::system_clock::time_point &timeNext) {
     time = std::chrono::system_clock::now();
     if (time >= timeNext) {
         timeNext = timeNext + std::chrono::seconds(updateMapTime);
-        sendDatasToServer("mct\n");
+        //sendDatasToServer("mct\n");
     }
 }
 
--- zappy_gui_src/DataManager/GameDataManager.cpp
+++ zappy_gui_src/DataManager/GameDataManager.cpp
@@ -58,7 +58,10 @@ void GameDataManager::addEgg(int id, int team, int x, int y) {
     std::lock_guard<std::mutex> lock(mutexDatas);
     Vec3d position = getTile(x, y).getWorldPos();
     position.Y += 0.2f;
-    eggs.emplace_back(id, team, MeshImporter::i().importMesh("DroneEgg", "",
+    std::string teamName = "";
+    printf("Adding egg with ID: %d, Team: %d (%s), Position: (%d, %d)\n",
+        id, team, teamName.c_str(), x, y);
+    eggs.emplace_back(id, team, MeshImporter::i().importMesh("DroneEgg", teamName,
         position, Vec3d(0.2f)));
 }
 

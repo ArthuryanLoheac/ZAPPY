47b1ae5cafd01e15e948363b210c4b8e9fc95c49
nathan bezard <nathan.bezard@epitech.eu>
2025-06-12 16:21:55 +0200

    🩹 USe new client structure

--- zappy_server_src/execution_command/fork_command.c
+++ zappy_server_src/execution_command/fork_command.c
@@ -16,14 +16,14 @@ static void lay_egg(zappy_t *zappy, client_t *client)
     int id_last_egg = zappy->map->eggs[0].id;
     egg_t *new_egg = malloc(sizeof(egg_t));
 
-    new_egg->x = client->x;
-    new_egg->y = client->y;
+    new_egg->x = client->stats.x;
+    new_egg->y = client->stats.y;
     new_egg->id = id_last_egg + 1;
-    new_egg->team_name = client->team_name;
+    new_egg->team_name = client->stats.team_name;
     new_egg->next = zappy->map->eggs;
     zappy->map->eggs = new_egg;
     sprintf(buffer, "enw #%d #%d %d %d", id_last_egg + 1,
-        client->id, new_egg->x, new_egg->y);
+        client->stats.id, new_egg->x, new_egg->y);
     send_data_to_graphics(zappy, buffer);
 }
 

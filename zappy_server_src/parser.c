3e85b21651ab6e62de28f5d44119cb0d189a4e65
JeanBizeul <jean.bizeul@epitech.eu>
2025-06-12 11:11:20 +0200

    ♻️ Replace -d by verbose level (-v, -vv, -vvv) in the server

--- zappy_server_src/parser.c
+++ zappy_server_src/parser.c
@@ -110,7 +110,11 @@ static void parse_one_arg(int ac, char **av, parser_t *parser, int i)
         parser->clients_per_team = parse_int(av[i + 1], 1, 200);
     if (strcmp(av[i], "-f") == 0 && i + 1 < ac)
         parser->freq = parse_int(av[i + 1], 1, 10000);
-    if (strcmp(av[i], "-d") == 0)
+    if (strcmp(av[i], "-v") == 0)
+        set_log_level(WARNING);
+    if (strcmp(av[i], "-vv") == 0)
+        set_log_level(INFO);
+    if (strcmp(av[i], "-vvv") == 0)
         set_log_level(DEBUG);
 }
 

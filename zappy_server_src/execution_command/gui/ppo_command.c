594b2d5a911380d6f12f04a903023db66cfb9ae9
JeanBizeul <jean.bizeul@epitech.eu>
2025-06-12 16:20:31 +0200

    🐛 Was trying to send gui position instead of asked player

--- zappy_server_src/execution_command/gui/ppo_command.c
+++ zappy_server_src/execution_command/gui/ppo_command.c
@@ -6,21 +6,39 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "command_handler.h"
 #include "pointlen.h"
 #include "logs.h"
 
-void ppo_command(zappy_t *zappy, client_t *client, char **args)
+void send_player_pos(zappy_t *zappy, client_t *gui, int id)
 {
     char response[20];
 
-    (void) zappy;
+    for (client_t *actual = zappy->clients; actual != NULL;
+        actual = actual->next) {
+        if (actual->stats.id == id) {
+            snprintf(response, 19, "ppo %i %i %i %i\n", actual->stats.id,
+                actual->stats.x, actual->stats.y, actual->stats.orientation);
+            add_to_buffer(&gui->out_buffer, response);
+            return;
+        }
+    }
+    LOG_WARNING("[%i]: Unknown player id(%i) from ppo gui request",
+        gui->fd, id);
+}
+
+void ppo_command(zappy_t *zappy, client_t *client, char **args)
+{
+    int id;
+
+    (void) client;
     if (pointlen(args) != 2) {
         LOG_WARNING("[%i]: Wrong amount of arguments for command ppo."
             " Got %i but required %i", client->fd, pointlen(args), 2);
     } else {
-        snprintf(response, 19, "ppo %i %i %i %i\n", client->stats.id,
-            client->stats.x, client->stats.y, client->stats.orientation);
+        id = atoi(args[1]);
+        send_player_pos(zappy, client, id);
     }
 }

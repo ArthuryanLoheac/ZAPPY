9cddf0b0e0a5209ad7b5b607bad680f35f57317a
JeanBizeul <jean.bizeul@epitech.eu>
2025-06-12 17:18:43 +0200

    🐛 Command TNA now correctly sends all team names instead of just GRAPHICAL

--- zappy_server_src/execution_command/gui/tna_command.c
+++ zappy_server_src/execution_command/gui/tna_command.c
@@ -11,21 +11,20 @@
 
 #include "command_handler.h"
 
-static void send_team_name(client_t *client)
+static void send_team_name(client_t *gui, const char *team_name)
 {
-    char response[6 + strlen(client->stats.team_name)];
+    char response[6 + strlen(team_name)];
 
-    snprintf(response, 5 + strlen(client->stats.team_name),
-        "tna %s\n", client->stats.team_name);
-    add_to_buffer(&client->out_buffer, response);
+    snprintf(response, 5 + strlen(team_name),
+        "tna %s\n", team_name);
+    add_to_buffer(&gui->out_buffer, response);
 }
 
 void tna_command(zappy_t *zappy, client_t *client, char **args)
 {
     (void) args;
     (void) client;
-    for (client_t *actual = zappy->clients; actual != NULL;
-        actual = actual->next) {
-        send_team_name(client);
+    for (int i = 0; zappy->parser->team_names[i]; i++) {
+        send_team_name(client, zappy->parser->team_names[i]);
     }
 }

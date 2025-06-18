9673973f8cce821ed7a9404f9d53717cbd6a8de3
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-06-18 19:22:36 +0200

    🐛 Fix client command handling by removing redundant connection check

--- zappy_server_src/command.c
+++ zappy_server_src/command.c
@@ -73,8 +73,7 @@ void handle_client_command(zappy_t *zappy, int fd)
     if (current == NULL)
         return;
     if (get_client_buffer(current, fd, zappy) == false) {
-        if (!current->is_graphic && current->is_connected &&
-            !current->is_waiting_id) {
+        if (!current->is_graphic && !current->is_waiting_id) {
             sprintf(buffer, "pdi #%d\n", current->stats.id);
             send_data_to_graphics(zappy, buffer);
         }

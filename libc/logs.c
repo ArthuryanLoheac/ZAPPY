86eb3ecfb0dee5de525eecfafed0b30d6645bcf2
JeanBizeul <jean.bizeul@epitech.eu>
2025-06-18 19:50:28 +0200

    ♻️ Use | instead of # to replace \n chars in the logs

--- libc/logs.c
+++ libc/logs.c
@@ -29,7 +29,7 @@ static void remove_newlines(char *str)
 {
     for (int i = 0; str[i] != '\0'; i++)
         if (str[i] == '\n')
-            str[i] = '#';
+            str[i] = '|';
 }
 
 // This function has 5 arguments to allow more modularity in the logs

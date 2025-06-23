001a34795766179bcc1f4fd8f543062a98e7b42d
mathys-f <mathys.fillion@epitech.eu>
2025-06-23 16:04:38 +0200

    ✅ Add unit tests for client and server functionalities

--- tests/test_map_update.c
+++ tests/test_map_update.c
@@ -11,3 +11,15 @@ static void redirect_all_std(void)
     cr_redirect_stderr();
 }
 
+Test(update_map, update_map_simple, .init = redirect_all_std)
+{
+    parser_t parser = {0};
+    cell_t **map = NULL;
+
+    parser.width = 10;
+    parser.height = 10;
+    map = create_map(&parser);
+    map[0][0].nbr_food = 0;
+    update_map(map, &parser);
+    destroy_map(map);
+}

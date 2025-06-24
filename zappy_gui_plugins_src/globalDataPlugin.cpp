0933f593a3d117a40ab2666f577325257f3c59d9
Arthuryan Lohéac <146721013+ArthuryanLoheac@users.noreply.github.com>
2025-06-24 19:28:48 +0200

    :sparkles: Add missing infos (#265)
    * ✨ Enhance global data display with team player counts and levels
    
    * ✨ Add player counting methods to global data plugin

--- zappy_gui_plugins_src/globalDataPlugin.cpp
+++ zappy_gui_plugins_src/globalDataPlugin.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <iostream>
 #include <cstdio>
 
@@ -21,19 +22,74 @@ irr::video::IVideoDriver* driver) {
 
     if (!font)
         return;
-    drawImage("assets/UI/BottomRight.png", 0, 0, 150,
-        80 + data.teams.size() * 30, driver);
+    int sizeY = 120 + data.teams.size() * 30;
+    for (size_t i = 0; i < data.teams.size(); i++)
+        sizeY += 20 * countNbLevelDifferentTeams(data.teams[i]);
+    drawImage("assets/UI/BottomRight.png", 0, 0, 200, sizeY, driver);
     // FPS
     font->draw(("FPS : " + std::to_string(driver->getFPS())).c_str(),
         UIRect(x, y, 300, 50), white);
+    // MAP
+    y += spaceBetween;
+    font->draw(("MAP (" + std::to_string(data.width) + "x" +
+        std::to_string(data.height) + ")").c_str(),
+        UIRect(x, y, 300, 50), white);
     // TEAMS
     y += spaceBetween;
-    font->draw("TEAMS : ", UIRect(x, y, 300, 50), white);
+    font->draw(("TEAMS (" + std::to_string(data.players.size()) + ") :")
+        .c_str(), UIRect(x, y, 300, 50), white);
     int i = 0;
     for (auto &team : data.teams) {
         y += 20;
-        font->draw(("\t" + team).c_str(), UIRect(x, y, 300, 50),
+        std::string teamStr = "\t" + team + " (" +
+            std::to_string(countNbPlayersOnTeam(team)) + ")";
+        font->draw(teamStr.c_str(), UIRect(x, y, 300, 50),
             data.teamColors[i % data.teamColors.size()]);
+        for (int level = 1; level <= 8; level++) {
+            int count = countNbPlayersOnTeam(team, level);
+            if (count > 0) {
+                y+= 20;
+                std::string levelStr = "\t\tLevel " + std::to_string(level) +
+                    " : " + std::to_string(count);
+                font->draw(levelStr.c_str(), UIRect(x + 20, y, 300, 50),
+                    data.teamColors[i % data.teamColors.size()]);
+            }
+        }
         i++;
     }
+}
+
+int globalDataPlugin::countNbPlayersOnTeam(const std::string &teamName) {
+    int count = 0;
+    for (const auto &player : data.players) {
+        if (player.teamName == teamName) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int globalDataPlugin::countNbPlayersOnTeam(const std::string &teamName,
+int level) {
+    int count = 0;
+    for (const auto &player : data.players) {
+        if (player.teamName == teamName && player.level == level) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int globalDataPlugin::countNbLevelDifferentTeams(const std::string &teamName) {
+    std::vector<int> levels;
+
+    for (const auto &player : data.players) {
+        if (player.teamName == teamName) {
+            if (std::find(levels.begin(), levels.end(), player.level) ==
+                levels.end()) {
+                levels.push_back(player.level);
+            }
+        }
+    }
+    return levels.size();
 }

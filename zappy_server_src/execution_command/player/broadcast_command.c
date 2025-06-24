52111be28881999d788c7c68c02ad7f4445901bd
Arthuryan Loheac <arthuryan.loheac-de-sainte-marie@epitech.eu>
2025-06-24 18:13:58 +0200

    🐛 Refactor broadcast_command to use dynamic memory allocation for textBuffer and improve buffer handling

--- zappy_server_src/execution_command/player/broadcast_command.c
+++ zappy_server_src/execution_command/player/broadcast_command.c
@@ -137,22 +137,16 @@ static void broadcast_every_client(zappy_t *zappy, client_t *client,
  */
 void broadcast_command(zappy_t *zappy, client_t *client, char **args)
 {
-    char textBuffer[2560];
+    char *textBuffer = malloc(sizeof(char) * 256);
 
-    for (int i = 0; i < 2560; i++)
-        textBuffer[i] = '\0';
     if (!args || args[0] == NULL) {
         add_to_buffer(&client->out_buffer, "ko\n");
         return;
     }
     for (int i = 0; args[i] != NULL; i++) {
         if (i > 0)
-            strcat(textBuffer, " ");
-        if (strlen(textBuffer) + strlen(args[i]) >= 2560) {
-            add_to_buffer(&client->out_buffer, "ko\n");
-            return;
-        }
-        strcat(textBuffer, args[i]);
+            add_to_buffer(&textBuffer, " ");
+        add_to_buffer(&textBuffer, args[i]);
     }
     broadcast_every_client(zappy, client, textBuffer);
 }
